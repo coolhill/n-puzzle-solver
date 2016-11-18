ae84bb23e8a14649240a041eec51519e5c2a1083
cogsad <cogsad@protonmail.com>
2016-11-18 23:25:50 +0000

    early stages of gui

--- gui.py
+++ gui.py
@@ -1,20 +1,30 @@
 from tkinter import *
 from tkinter import ttk
+import solver
+
+
+gui = Tk()
+gui.title("GUI")
+gui.geometry('1200x700')
+
+
+#debug and testing data untill actual data is obtainable 
+def insert():
+    data = 56
+    text.insert(END,(data))
+    text.insert(END,(solver.start))
     
-root = Tk()
-root.title("GUI")
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
+b1 = Button(text = 'Graph', height = 1, width = 5)
+b1.place(relx = .95, rely = .85, anchor = "c")
+
+b2 = Button(gui, text = 'Solve', height = 1, width = 5, command =lambda:  insert())
+b2.place(relx = .30, rely = .70, anchor = "c") 
+
+text = Listbox(height = 25, width = 75)
+text.place (relx = .50, rely = .30, anchor = "c")
 
 
+gui.bind('<Return>')
 
-ttk.Button(mainframe, text="Graph").grid(column=3, row=3, sticky=W)
-
-for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
-
-root.bind('<Return>')
-
-root.mainloop()
+gui.mainloop()
